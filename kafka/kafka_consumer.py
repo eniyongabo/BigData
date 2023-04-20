c8737bbe51d8518e66624cf5ce8b6d85fd147347
Jocelin Hounon <hounonj@gmail.com>
2023-04-20 10:59:08 -0500

    update

--- kafka/kafka_consumer.py
+++
@@ -1,7 +0,0 @@
-from kafka import KafkaConsumer
-
-consumer = KafkaConsumer(
-    "foobar", auto_offset_reset="earliest", bootstrap_servers=["localhost:9092"]
-)
-for msg in consumer:
-    print(msg)
